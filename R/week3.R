9ec109f6ee0de6e6b623ad626e1d09772ccaa38b
Michael Hazboun <hazbo002@umn.edu>
2024-02-01 18:00:13 -0600

    finished line 8 and long comment about it

--- R/week3.R
+++ R/week3.R
@@ -3,5 +3,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import and Cleaning
 raw_df <- read.csv(file="../Data/week3.csv")
-
-
+raw_df$timeStart<- as.POSIXct(raw_df$timeStart)
+raw_df$timeEnd<- as.POSIXct(raw_df$timeEnd)
+clean_df<- raw_df[raw_df$timeStart >= "2017-07-01 00:00:00" | raw_df$timeStart < "2017-06-01 00:00:00", ] # it doesnt seem like anyone did anything before June, but this should be a more complete answer than if I simply had it be >2017..
+clean_df <- 
